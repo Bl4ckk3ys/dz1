3b1df4d7ed03a6aa8f6b122e35e84ac2027b4976
bl4ckk3ys <ilchechnev@gmail.com>
2023-10-04 20:41:01 +0300

    New commit

---
+++ dz1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, null;
+    scanf("%d %d", &n1, &n2);
+    printf("\n%d",(n1+n2));
+    printf("\n%d",(n1-n2));
+    printf("\n%d",(n1*n2));
+    printf("\n%d",(n1/n2));
+
+    scanf("\n%d",&null);
+    return 0;
+}
